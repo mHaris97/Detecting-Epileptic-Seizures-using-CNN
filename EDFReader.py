2470b47f18008844bc7be51e9b8997f50fa89506
Muhammad Haris <mrharisyousfani@hotmail.com>
2020-10-13 02:41:08 +0500

    data loading and reading done.

--- EDFReader.py
+++ EDFReader.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 signals, signal_headers, header = highlevel.read_edf('MIT EEG Data\chb01_01.edf')
 print(signal_headers[0]['sample_rate']) # prints 256
 
+##t = np.linspace(-2*np.pi, 2*np.pi, 500);
+##signals = np.stack((np.sin(t), np.sin(2*t), np.sin(3*t)))
+
 dft = np.fft.fft(signals, axis=1)
 
 plt.subplot(1,2,1)
-plt.plot(np.transpose(signals[0:3,:1000]))
+plt.plot(np.transpose(signals))#[0:3,:1000]))
 plt.title('Raw signals')
 plt.subplot(1,2,2)
-plt.plot(np.transpose(dft[0:3,:1000]))
+plt.plot(np.transpose(dft))#[0:3,:1000]))
 plt.title('Fourier transform')
 plt.show()
